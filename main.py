d705b32d3ad96e91097239991e81f6a01dccb766
Evgeniy321 <evgeniy.goh@gmail.com>
2024-09-26 13:50:00 +0500

    added concurrent.futures

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import asyncio
 
-from pandas import read_csv, json_normalize
+from pandas import json_normalize
 from aiohttp import ClientSession
 from json import dump
-from config import CSV_PATH, API_URL,RESULT_JSON_PATH
+from config import API_URL,RESULT_JSON_PATH
 
 
 def parce_json(data_json):#обработка ответа от API с помощю pandas
@@ -21,11 +21,8 @@ async def get_api_data(row: dict):
         async with session.get(API_URL) as resp: # [4]
             response = await resp.read() # [5]
             write_json(response['data'])
-def main():
-    data = read_csv(CSV_PATH)
-    data_dict = data.to_dict(orient='records')
-    for row in data_dict:
-        asyncio.run(get_api_data(row))
+def main(row):
+    asyncio.run(get_api_data(row))
 
 
 
--- workerScript.py
+++ workerScript.py
@@ -1,54 +1,78 @@
-from dask.distributed import LocalCluster
-from prefect import Flow, task, context
-from prefect.executors import DaskExecutor
-from prefect.engine.signals import FAIL
+import concurrent.futures
+import os
+import psutil
+from prefect.states import Completed, Failed
+from prefect.task_runners import ConcurrentTaskRunner
+from prefect import task, flow, context
+
+from pandas import read_csv
+
 from datetime import timedelta
+from main import main, read_csv
 from bot_handle import flow_success_handler, flow_failure_handler
+from config import CSV_PATH
 
-# Создаем локальный кластер Dask с ограничением ресурсов
-cluster = LocalCluster(
-    n_workers=2,            # Количество worker'ов
-    threads_per_worker=1,    # Количество потоков на worker
-    memory_limit="1GB"       # Лимит памяти на worker
-)
+def limit_worker_resources():
+    p = psutil.Process(os.getpid())
+    p.cpu_affinity([0])
+    p.nice(psutil.HIGH_PRIORITY_CLASS)
+    
+    try:
+        p.rlimit(psutil.RLIMIT_AS, (2 * 1024 * 1024 * 1024, psutil.RLIM_INFINITY))  # Лимит 2 ГБ(unix)
+    except AttributeError:
+        pass  
 
-# Создаем DaskExecutor с подключением к кластеру
-executor = DaskExecutor(cluster)
 
 # Задача с тайм-аутом, повторными попытками и логированием
 @task(timeout_seconds=30, retries=3, retry_delay=timedelta(seconds=10))
 def limited_task():
-    # Получаем logger из контекста Prefect
     logger = context.get("logger")
-    
+    limit_worker_resources()
     try:
         logger.info("Задача начата")
-        
-        import time
-        logger.info("Имитация длительного выполнения задачи...")
-        time.sleep(35)  # Задача, которая должна превысить лимит времени
-        
+        main()
         logger.info("Задача успешно выполнена")
         return "Task completed"
     
     except Exception as e:
-        # Логируем ошибку
         logger.error(f"Ошибка в задаче: {e}")
-        # Прерываем задачу с сигналом FAIL, чтобы Prefect использовал retries
-        raise FAIL("Ошибка: Тайм-аут или другое исключение")
+
+
+def parallel_processing():
+    data = read_csv(CSV_PATH)
+    data_dict = data.to_dict(orient='records')
+    # Используем ThreadPoolExecutor для выполнения задач в нескольких потоках
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        future_tasks = [executor.submit(limited_task.run, data) for data in data_dict]
+        results = [future.result() for future in concurrent.futures.as_completed(future_tasks)]
+    return results
+
+def task_callback(task, old_state, new_state):#колбеки для отправки сообщения в телеграмм 
+    if isinstance(new_state, Completed):
+        flow_success_handler(task, new_state)
+    elif isinstance(new_state, Failed):
+        flow_failure_handler(task, new_state)
+    return new_state
 
 # Создаем поток
-with Flow("dask-flow-with-logging-and-retries") as flow:
-    result = limited_task()
+@flow(name="Parallel-Processing-Flow", task_runner=ConcurrentTaskRunner())
+def main_flow():
+    data_list = [1, 2, 3, 4, 5]
+    results = []
+    for data in data_list:
+        # Привязываем колбэк к каждой задаче
+        task_state = limited_task.with_options(on_completion=task_callback)(data)
+        results.append(task_state)
+    return results
 
-flow.set_reference_tasks([result])#Привязка колбэков к состоянию потока
-flow.on_success(flow_success_handler)
-flow.on_failure(flow_failure_handler)
+    
 
-# Запуск потока с использованием DaskExecutor
 if __name__ == "__main__":
-    # Логирование запуска потока
     logger = context.get("logger")
     logger.info("Запуск потока...")
-    flow.run(executor=executor)
+    main_flow.run().deploy(
+        name="my-first-deployment",
+        work_pool_name="my-pool", # Work pool target
+        cron="0 1 * * *", # Cron schedule (1am every day)
+    )
     logger.info("Поток завершен")
